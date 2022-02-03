2ad1f3d7bc7f11e70bcc3de2a11966fd55bd048b
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-02-03 15:25:24 +0100

    DoubleSection

--- Asset/js/diapo.js
+++ Asset/js/diapo.js
@@ -1,1 +1,33 @@
 const videoJs = document.querySelectorAll(".videoJs");
+let delayDiapo = 5000;
+
+window.onload = () => {
+  affichage();
+};
+let i = 0;
+let flag = 1;
+
+//Fonction d'affichage
+const affichage = () => {
+  setInterval(() => {
+    //arrivé à 4 on retourne i à 0, et on met flag à 0
+    if (i == 4) {
+      i = 0;
+      flag = 0;
+    }
+    if (i == 0) {
+      //Si on à déja fait un tour on retire le dernier pour revenir dans le setup du début
+      if (flag == 0) {
+        videoJs[3].classList.toggle("display");
+        flag = 1;
+      }
+    }
+    //On enléve l'affichage de celui d'avant sauf si c'est déja 0
+    if (i != 0) {
+      videoJs[i - 1].classList.toggle("display");
+    }
+    videoJs[i].classList.toggle("display");
+
+    i++;
+  }, delayDiapo);
+};
--- Asset/js/script.js
+++ Asset/js/script.js
@@ -2,9 +2,39 @@ const navbar = document.getElementById("navbar");
 const tap_dropdown = document.getElementById("tap_dropdown");
 const dropdown = document.getElementById("dropdown");
 var dropdown_dropped = false;
+const videoJs = document.querySelectorAll(".videoJs");
+let delayDiapo = 5000;
+let i = 0;
+let flag = 1;
 
 window.onload = function () {
   set_dropdown();
+  affichage();
+};
+
+//Fonction d'affichage
+const affichage = () => {
+  setInterval(() => {
+    //arrivé à 4 on retourne i à 0, et on met flag à 0
+    if (i == 4) {
+      i = 0;
+      flag = 0;
+    }
+    if (i == 0) {
+      //Si on à déja fait un tour on retire le dernier pour revenir dans le setup du début
+      if (flag == 0) {
+        videoJs[3].classList.toggle("display");
+        flag = 1;
+      }
+    }
+    //On enléve l'affichage de celui d'avant sauf si c'est déja 0
+    if (i != 0) {
+      videoJs[i - 1].classList.toggle("display");
+    }
+    videoJs[i].classList.toggle("display");
+
+    i++;
+  }, delayDiapo);
 };
 
 function set_dropdown() {
@@ -46,3 +76,4 @@ tap_dropdown.onclick = function () {
   dropdown_dropped = !dropdown_dropped;
   set_dropdown();
 };
+/*--------*/
