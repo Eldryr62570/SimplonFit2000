e9ff357302615af667301021dfe72ff680180939
Jordan MOULIN <98018776+Eldryr62570@users.noreply.github.com>
2022-02-06 17:06:27 +0100

    Début petanque

--- Asset/js/paralax.js
+++ Asset/js/paralax.js
@@ -1,5 +1,7 @@
 const parallax1 = document.querySelector("#parallax1");
 const parralax2 = document.querySelector("#parallax2");
+const parralax2 = document.querySelector("#parallax3");
+
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
   parallax1.style.backgroundPositionY = offset * 0.03 + "px";
@@ -7,4 +9,8 @@ window.addEventListener("scroll", () => {
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
   parallax2.style.backgroundPositionY = offset * -0.03 + "px";
+});
+window.addEventListener("scroll", () => {
+  let offset = window.pageYOffset;
+  parallax3.style.backgroundPositionY = offset * -0.03 + "px";
 });
