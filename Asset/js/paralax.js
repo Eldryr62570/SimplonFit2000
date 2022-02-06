0adfa92e3cccd000d1906a6a25d8cfccc3ca2007
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-02-06 17:30:12 +0100

    petanque section terminé

--- Asset/js/paralax.js
+++ Asset/js/paralax.js
@@ -1,6 +1,6 @@
 const parallax1 = document.querySelector("#parallax1");
-const parralax2 = document.querySelector("#parallax2");
-const parralax2 = document.querySelector("#parallax3");
+const parrallax2 = document.querySelector("#parallax2");
+const parramlax2 = document.querySelector("#parallax3");
 
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
