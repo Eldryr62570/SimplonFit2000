209d86269c8a4ac5572a80107e3b29e12c9fa875
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-02-07 10:11:44 +0100

    Commit micro changement style

--- Asset/js/paralax.js
+++ Asset/js/paralax.js
@@ -1,6 +1,7 @@
 const parallax1 = document.querySelector("#parallax1");
 const parrallax2 = document.querySelector("#parallax2");
-const parramlax2 = document.querySelector("#parallax3");
+const parallax3 = document.querySelector("#parallax3");
+const parallax4 = document.querySelector("#parallax4");
 
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
@@ -13,4 +14,8 @@ window.addEventListener("scroll", () => {
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
   parallax3.style.backgroundPositionY = offset * -0.03 + "px";
+});
+window.addEventListener("scroll", () => {
+  let offset = window.pageYOffset;
+  parallax4.style.backgroundPositionY = offset * -0.03 + "px";
 });
